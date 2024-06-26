be3a3e19dc2b97156a442370a375c307a717e901
lida-skakun <kunska@gmail.com>
2024-06-26 13:31:46 +0200

    Created hooks for storing username in local storage, added layout to the routing

---
+++ src/services/localStorage.js
@@ -0,0 +1,25 @@
+const LS_KEYS = {
+  USER: "user",
+};
+
+class LocalStorageService {
+  static get(key) {
+    const value = window.localStorage.getItem(key);
+    try {
+      return JSON.parse(value);
+    } catch {
+      return value;
+    }
+  }
+  static set(key, value) {
+    return window.localStorage.setItem(key, JSON.stringify(value));
+  }
+  static remove(key) {
+    return window.localStorage.removeItem(key);
+  }
+  static clearAll() {
+    return window.localStorage.clear();
+  }
+}
+
+export { LocalStorageService, LS_KEYS };
