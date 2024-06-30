65743cb3b14a45631b9a064aca00689b448a20b0
lida-skakun <kunska@gmail.com>
2024-06-30 15:16:26 +0200

    Iported values from cert lokal ctorage, added Cart peovider to the app

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -13,16 +13,33 @@ import { LocalStorageService, LS_KEYS } from "../services/localStorage";
 import { UserProvider } from "../hooks/use-user";
 import defaultBooks from "../data/books.json";
 import "./app.scss";
+import { CartProvider } from "../hooks/use-cart";
 
 function App() {
-  const [items, setItems] = useState(defaultBooks.books);
+  const [bookList, setBookList] = useState(defaultBooks.books);
+
   const [user, setUser] = useState(
     LocalStorageService.get(LS_KEYS.USERS) || ""
   );
 
+  const [cartItems, setCartItems] = useState(
+    LocalStorageService.get(LS_KEYS.CART) || {}
+  );
+  {
+    /* useEffect(() => {
+    const getDatas = async () => {
+      const response = await fetch("../data/books.json");
+      const data = await response.json();
+      setBookList(data);
+    };
+    getDatas();
+  }, []);
+  */
+  }
   useEffect(() => {
-    LocalStorageService.set(LS_KEYS.USER, user);
-  }, [user]);
+    LocalStorageService.set(LS_KEYS.USERS, user);
+    LocalStorageService.set(LS_KEYS.CART, cartItems);
+  }, [user, cartItems]);
 
   function RequireAuth({ children }) {
     return user ? children : <Navigate to="/" />;
@@ -31,40 +48,42 @@ function App() {
   return (
     <div className="App">
       <UserProvider value={{ user, setUser }}>
-        <ItemsProvider value={{ items, setItems }}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<SignInPage />} />
-                <Route
-                  path="book-list"
-                  element={
-                    <RequireAuth>
-                      <BookListPage />
-                    </RequireAuth>
-                  }
-                />
-                <Route
-                  path="specific-book"
-                  element={
-                    <RequireAuth>
-                      <SpecificBookPage />
-                    </RequireAuth>
-                  }
-                />
-                <Route
-                  path="/cart"
-                  element={
-                    <RequireAuth>
-                      <CartPage />
-                    </RequireAuth>
-                  }
-                />
-                <Route path="*" element={<NotFoundPage />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </ItemsProvider>
+        <CartProvider value={{ cartItems, setCartItems }}>
+          <ItemsProvider value={{ bookList, setBookList }}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<SignInPage />} />
+                  <Route
+                    path="book-list"
+                    element={
+                      <RequireAuth>
+                        <BookListPage />
+                      </RequireAuth>
+                    }
+                  />
+                  <Route
+                    path="/specific-book"
+                    element={
+                      <RequireAuth>
+                        <SpecificBookPage />
+                      </RequireAuth>
+                    }
+                  />
+                  <Route
+                    path="/cart"
+                    element={
+                      <RequireAuth>
+                        <CartPage />
+                      </RequireAuth>
+                    }
+                  />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </ItemsProvider>
+        </CartProvider>
       </UserProvider>
     </div>
   );
