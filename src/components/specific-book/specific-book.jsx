daa530f6d86234a39d6d9f457a3b58d2fcfd0155
lida-skakun <kunska@gmail.com>
2024-07-01 13:41:06 +0200

    Added filtered book, details and description  of the book added to the page

--- src/components/specific-book/specific-book.jsx
+++ src/components/specific-book/specific-book.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,9 +7,14 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import Form from "react-bootstrap/Form";
+import { useItems } from "../../hooks/use-items";
 import "./specific-book.scss";
 
-export default function SpecificBook({ books }) {
+export default function SpecificBook() {
+  const { id } = useParams();
+  const books = useItems().bookList;
+  const selectedBook = books.find((book) => book.id == id);
+
   return (
     <>
       <Container className="mt-5 mb-5">
@@ -16,37 +22,39 @@ export default function SpecificBook({ books }) {
           <Col xs={4}>
             <Carousel fade data-bs-theme="dark" className="carouselImage">
               <Carousel.Item>
-                <img src={books[0].image} />
+                <img src={selectedBook.image} />
               </Carousel.Item>
               <Carousel.Item>
-                <img src={books[4].image} />
+                <img src={selectedBook.image} />
               </Carousel.Item>
               <Carousel.Item>
-                <img src={books[5].image} />
+                <img src={selectedBook.image} />
               </Carousel.Item>
             </Carousel>
           </Col>
           <Col xs={4} className="aboutBookPanel">
-            <h4>Book name, book author</h4>
+            <h4>
+              {selectedBook.title}, {selectedBook.author}
+            </h4>
             <ul>
               <li>
                 <b>Book name: </b>
-                <span>KKKKKK kkkkkk</span>
+                <span>{selectedBook.title}</span>
               </li>
               <li>
                 <b>Book author: </b>
-                <span>KKKKdKK kkkkkk</span>
+                <span>{selectedBook.author}</span>
               </li>
               <li>
                 <b>Book level: </b>
-                <span>KKKKKK kkkkkk</span>
+                <span>{` ${selectedBook.level}`}</span>
               </li>
               <li>
-                <b>Book tags: </b>
-                <span>KKKKKK kkkkkk</span>
+                <b>Book tags:</b>
+                <span>{` ${selectedBook.tags}`}</span>
               </li>
             </ul>
-            <p id="bookPrice">78.67$</p>
+            <p id="bookPrice">{selectedBook.price}$</p>
           </Col>
           <Col xs={4}>
             <Card id="purchasePanel">
@@ -55,7 +63,7 @@ export default function SpecificBook({ books }) {
               <ul>
                 <li>
                   <span>Price, $</span>
-                  <span id="bookPriceInCart"></span>
+                  <span id="bookPriceInCart">{selectedBook.price}</span>
                 </li>
                 <li>
                   <span className="countQuantity">Count, pc</span>
@@ -104,13 +112,7 @@ export default function SpecificBook({ books }) {
         <Row>
           <Col xs={8} className="bookDescription mt-5 p-5">
             <h5>Description:</h5>
-            <p>
-              A book providing an introduction to thr JavaScript language and
-              programming in general Lorem ipsum dolor, sit amet consectetur
-              adipisicing elit. Est dolorem laborum, illum consectetur provident
-              harum nostrum officia minima cum laudantium a ratione et! Dolorum,
-              expedita quibusdam. Ducimus ea explicabo eligendi.
-            </p>
+            <p>{selectedBook.description}</p>
           </Col>
         </Row>
       </Container>
