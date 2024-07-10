030e10bbc98bfffa1f8ed27120a66fd74dd26716
lida-skakun <kunska@gmail.com>
2024-07-10 10:29:15 +0200

    Cart count items added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,25 +1,47 @@
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate, Link } from "react-router-dom";
 import { useUser } from "../../hooks/use-user";
+import { useCart } from "../../hooks/use-cart";
 import cartIcon from "../../img/cart.svg";
 import avatar from "../../img/avatar.png";
 import "./header.scss";
 
 export function Header() {
   const { user, setUser } = useUser();
+  const { cart } = useCart();
+  const [booksInCart, setBooksInCart] = useState(0);
   const navigate = useNavigate();
   const handleClick = () => {
     setUser("");
     navigate("/");
   };
 
+  useEffect(() => {
+    if (cart.addedBooks) {
+      setBooksInCart(
+        cart.addedBooks.reduce(
+          (acc, currentValue) => acc + Number(currentValue.quantity),
+          0
+        )
+      );
+    } else {
+      setBooksInCart(0);
+    }
+  }, [cart]);
+
   let addClass = window.frames.location.pathname === "/" ? "hiddenBlock" : "";
+
+  const quantityOfBooksInCart = booksInCart > 0 && (
+    <span className="quantityOfBooksInCart">{booksInCart}</span>
+  );
 
   return (
     <div className="navigationPanel">
       <h4>My book store / Lidiia Tkachova</h4>
       <div className={`controlPanel ${addClass}`}>
-        <Link to="/cart">
+        <Link to="/cart" className="linkToBasket">
+          {quantityOfBooksInCart}
           <img src={cartIcon} alt="image 0f bucket" id="basketImage" />
         </Link>
         <Button variant="dark" onClick={handleClick}>
