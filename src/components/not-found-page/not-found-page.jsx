ef42190e58c7a2b9227570abbf9ce958f689b704
lida-skakun <kunska@gmail.com>
2024-07-24 12:59:30 +0200

    Added spacing to the error page

--- src/components/not-found-page/not-found-page.jsx
+++ src/components/not-found-page/not-found-page.jsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
   return (
-    <div className="NotFoundPage">
+    <div className="NotFoundPage text-center mt-5">
       <h1>Something went wrong (404)</h1>
     </div>
   );
